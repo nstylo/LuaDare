5a0f8da72ccb430eda9e683c62bebc8d2022b407
MrCappuccino <MrCappuccino@users.noreply.github.com>
2019-04-27 21:24:05 +0200

    cleanup MapGenerator, add comments

--- MapGenerator.lua
+++ MapGenerator.lua
@@ -17,15 +17,16 @@ function MapGenerator:new(sizeX, sizeY, numWalkers, cellsize)
     this.sizeY = sizeY
     this.cellsize = cellsize
 
-    this.numWalkPath = 0
-    this.minWalkers = 1
-    this.maxWalkers = 10
-    this.startWalkers = numWalkers
+    this.numFloors = 0 -- number of floors
+    this.minWalkers = 1 -- minimum number of walkers allowed to exist
+    this.maxWalkers = 5 -- maximum number of walkers allowed to exist
+    this.startWalkers = numWalkers -- The starting amount of walkers
 
     -- assign an empty grid
     local grid = {}
     this.grid = grid
 
+    -- initializes the grid (all walls)
     for i = 1, sizeX do grid[i] = {}
         for j = 1, sizeY do
             grid[i][j] = 0
@@ -40,10 +41,10 @@ function MapGenerator:new(sizeX, sizeY, numWalkers, cellsize)
     local x = math.floor(sizeX / 2)
     local y = math.floor(sizeY / 2)
 
+    -- initialize the walkers
     for k = 1, this.startWalkers do
         walkers[k] = {x = x, y = y}
     end
-
 
     return setmetatable(this, metatable)
 end
@@ -60,6 +61,7 @@ end
 function MapGenerator:exportToFile(filename)
     local file = io.open("test.txt", "w+")
 
+    -- write to map file from "grid"
     for i = 1, self.sizeX do
         for j = 1, self.sizeY do
             if j == self.sizeY then -- last grid element
@@ -69,7 +71,7 @@ function MapGenerator:exportToFile(filename)
             end
         end
     end
-    file:seek("set", 0)
+
     file:close()
 end
 
@@ -93,7 +95,7 @@ function MapGenerator:doDrunkardsMove(w)
     -- check if current grid index is a 0, if so, then change to 1 and increment counter
     if self.grid[walker.x][walker.y] == 0 then
         self.grid[walker.x][walker.y] = 1
-        self.numWalkPath = self.numWalkPath + 1 -- increment number of non 0s
+        self.numFloors = self.numFloors + 1 -- increment number of non 0s
     end
 end
 
@@ -101,7 +103,8 @@ end
 function MapGenerator:delWalker()
     local chanceWalkerDel = 0.001
 
-    for i=#self.walkers,1,-1 do
+    -- loop through walkers, remove based on random chance
+    for i = 1, #self.walkers do
         if math.random() < chanceWalkerDel and #self.walkers > self.minWalkers then
             table.remove(self.walkers, i) -- remove the walker
             do return end
@@ -113,7 +116,8 @@ end
 function MapGenerator:addWalker()
     local chanceWalkerAdd = 0.001
 
-    for i=1, #self.walkers do
+    -- loop through walkers, add based on random chance
+    for i = 1, #self.walkers do
         if math.random() < chanceWalkerAdd and #self.walkers < self.maxWalkers then
             local walker = {} -- Create new walker
             -- Set new walker's x and y coords to i walker's
@@ -131,30 +135,32 @@ function MapGenerator:clearSpawn()
     local middleY = math.floor(self.sizeY / 2)
 
     -- clear a 20x20 area at spawn
-    for i=(middleX-10), (middleX+10) do
-        for j=(middleY-10), (middleY+10) do
+    for i = (middleX - 5), (middleX + 5) do
+        for j = (middleY - 5), (middleY + 5) do
             self.grid[i][j] = 1 -- set the points to a path
         end
     end
 end
 
--- param perc : the percentage covered with walking paths
-function MapGenerator:doDrunkardsWalk(perc)
-    -- iter is given in percentage
-    if perc > 1 then
-        perc = 1
-    elseif perc < 0 then
-        perc = 0
+-- param ratio : the percentage covered with walking paths
+function MapGenerator:doDrunkardsWalk(ratio)
+    -- ratio [0, 1]
+    if ratio > 1 then
+        ratio = 1
+    elseif ratio < 0 then
+        ratio = 0
     end
 
     -- walk with a walker until we find a tile to set to 1
     -- until the desired percentage is reached
-    while (self.numWalkPath / (self.sizeX * self.sizeY)) < perc do
+    while (self.numFloors / (self.sizeX * self.sizeY)) < ratio do
+        -- randomly add and remove walkers based on chance
         self:delWalker()
         self:addWalker()
-        self:doDrunkardsMove((self.numWalkPath % #self.walkers) + 1)
+        self:doDrunkardsMove((self.numFloors % #self.walkers) + 1) -- generate floors with drunkards
     end
-    self:clearSpawn()
+
+    self:clearSpawn() -- make a clearing for the spawn area
 end
 
 return MapGenerator
