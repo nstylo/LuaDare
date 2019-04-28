520ec754f4fd9b4a26155d92747ad2eedc8d7e34
Juris Majors <jurismajors@gmail.com>
2019-04-28 21:56:21 +0200

    player class

--- main.lua
+++ main.lua
@@ -1,9 +1,7 @@
-require("player")
-
 function tmpGunBullet()
     local bullet = {}
     bullet.b = love.physics.newBody(world, 10, 10, "dynamic")
-    bullet.s = love.physics.newCircleShape(objects.head.shape:getRadius() * 0.5)
+    bullet.s = love.physics.newCircleShape(player.shape:getRadius() * 0.5)
     bullet.f = love.physics.newFixture(bullet.b, bullet.s) -- add physics
     bullet.f:setRestitution(0.2) -- determine how bouncy this be
     bullet.b:setActive(false)
@@ -12,6 +10,10 @@ function tmpGunBullet()
 end
 
 function love.load()
+    -- player constants
+    local PLAYER_VELOCITY = 200
+    local HEALTH = 100
+
     love.physics.setMeter(64)
     world = love.physics.newWorld(0, 0, true)
     world:setCallbacks(beginContact, endContact, preSolve, postSolve)
@@ -23,25 +25,25 @@ function love.load()
     mapgen = MapGenerator:new(80, 80, 5, 32)
     mapgen:doDrunkardsWalk(0.05)
     mapgen:exportToFile("test.txt")
-
     -- centre of map
     mapCenterX = mapgen.sizeX * mapgen.cellsize / 2
     mapCenterY = mapgen.sizeY * mapgen.cellsize / 2
 
-    objects = {}
-    -- stores objects to draw and physics
-    objects.static = {} -- static world objects
-    objects.head= {} -- player
-    initializePlayer(objects.head, mapCenterX, mapCenterY)
-    initializeMap(objects.static)
-
+    -- create gun
     GunCreator = require("gun")
-
     tmpGun_bullet_creator = {
         create = tmpGunBullet
     }
 
     tmpGun = GunCreator:new(0.3, 0.5, 9, 13,"assets/sounds/gun_fire.wav", tmpGun_bullet_creator)
+
+    PlayerCreator = require("player")
+    player = PlayerCreator:new(PLAYER_VELOCITY, tmpGun, mapCenterX, mapCenterY, HEALTH, world)
+
+    objects = {}
+    -- stores objects to draw and physics
+    objects.static = {} -- static world objects
+    initializeMap(objects.static)
 
     -- contains the bullets
     objects.bullets = {} -- contains bullets currently flying
@@ -92,39 +94,34 @@ end
 function love.update(dt)
     world:update(dt)
     tmpGun:update(dt)
+
     if t < shakeDuration then
         t = t + dt
     end
 
-    headbody = objects.head.body -- player body
-    kybrd = love.keyboard -- keyboard object
+    local kybrd = love.keyboard -- keyboard object
     mouse = love.mouse
-    -- get current position
-    headX, headY = headbody:getPosition()
-    -- get current velocity
-    curX, curY = headbody:getLinearVelocity()
-    -- update player angle and velocity
-    headbody:setLinearVelocity(getPlayerVelocity(curX, curY, kybrd))
+    player:update(dt, kybrd)
+    local curGun = player:getGun()
 
-    if tmpGun:shouldShoot(mouse) then
+    if curGun:shouldShoot(mouse) then
         local translateX, translateY = getTranslate()
         bulletCount = (bulletCount + 1) % 10000
-        local bullet = tmpGun:shoot(headbody,
-            objects.head.shape:getRadius(),
-            mouse:getX() + math.abs(translateX),
-            mouse:getY() + math.abs(translateY)
-            )
+
+        local bullet = curGun:shoot(player.body,
+                        player.shape:getRadius(),
+                        mouse:getX() + math.abs(translateX),
+                        mouse:getY() + math.abs(translateY))
+
         bullet.f:setUserData(tostring(bulletCount))
         objects.bulletTouching[tostring(bulletCount)] = 0
         table.insert(objects.bullets, bullet)
     end
 
-    processBullets(tmpGun.speed)
-    -- dont rotate the player
-    headbody:setAngularVelocity(0)
+    processBullets(curGun.speed)
 
     for i = 1, 3 do
-        objects.enemies[i]:update(headX, headY)
+        objects.enemies[i]:update(player.body:getX(), player.body:getY())
     end
 
 end
@@ -145,7 +142,7 @@ end
 
 -- gets camera translation to map from screen space to world space
 function getTranslate()
-    return -objects.head.body:getX() + love.graphics.getWidth()/2, -objects.head.body:getY() + love.graphics.getHeight()/2
+    return -player.body:getX() + love.graphics.getWidth()/2, - player.body:getY() + love.graphics.getHeight()/2
 end
 
 function love.draw()
@@ -153,10 +150,10 @@ function love.draw()
     love.graphics.reset()
 
     -- screen bounds in world space
-    local minBoundX = math.floor(objects.head.body:getX() - love.graphics:getWidth() / 2 - mapgen.cellsize)
-    local minBoundY = math.floor(objects.head.body:getY() - love.graphics:getHeight() / 2 - mapgen.cellsize)
-    local maxBoundX = math.floor(objects.head.body:getX() + love.graphics:getWidth() / 2 + mapgen.cellsize)
-    local maxBoundY = math.floor(objects.head.body:getY() + love.graphics:getHeight() / 2  + mapgen.cellsize)
+    local minBoundX = math.floor(player.body:getX() - love.graphics:getWidth() / 2 - mapgen.cellsize)
+    local minBoundY = math.floor(player.body:getY() - love.graphics:getHeight() / 2 - mapgen.cellsize)
+    local maxBoundX = math.floor(player.body:getX() + love.graphics:getWidth() / 2 + mapgen.cellsize)
+    local maxBoundY = math.floor(player.body:getY() + love.graphics:getHeight() / 2  + mapgen.cellsize)
 
     -- move according to player
     love.graphics.translate(getTranslate())
@@ -167,8 +164,7 @@ function love.draw()
     -- shake the screen
     --shakeScreen()
 
-    --love.graphics.translate(math.floor(objects.head.body:getX() - love.graphics.getWidth() / 2), math.floor(objects.head.body:getY() - love.graphics.getHeight() / 2))
-    local headbody = objects.head.body
+    local headbody = player.body
     xH, yH = headbody:getPosition()
     xH = xH - love.graphics.getWidth() / 2
     yH = yH - love.graphics.getHeight() / 2
@@ -177,9 +173,7 @@ function love.draw()
 	    objects.enemies[i]:draw(xH, yH)
     end
 
-    love.graphics.setColor(1,1,1)
-    -- draw the player
-    love.graphics.circle("line", objects.head.body:getX() , objects.head.body:getY(), objects.head.shape:getRadius())
+    player:draw()
 end
 
 function initializePlayer(playerContainer, playerX, playerY)
